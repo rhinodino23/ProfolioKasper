f138b2061d8744e98e6ecac68b0e61c24fd515a0
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-27 22:36:36 -0500

    Connecting all of the pages.

--- route/pages.js
+++ route/pages.js
@@ -1,6 +1,20 @@
 const express = require('express');
-const router = express.Router{};
+const router = express.Router();
 
-router.get('/', {reg, res} =>{
+router.get('/', (reg, res) =>{
     res.render('index')
 })
+router.get('/contact', (reg, res) =>{
+    res.render('contact')
+})
+router.get('/experience', (reg, res) =>{
+    res.render('experience')
+})
+router.get('/education', (reg, res) =>{
+    res.render('education')
+})
+router.get('/programming_languages', (reg, res) =>{
+    res.render('programming_languages')
+})
+
+module.exports=router;
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const route = require('./route/pages')
+const path = require('path')
 const app = express();
 
 app.set('view-engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static(path.join(__dirname,'Windows')))
 app.use('/', route);
 
-mongoose.connect("mongodb+srv://JuniorGhost:FurBaby32@cluster0.w0f6fzl.mongodb.net/portfoliodb",{userNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://JuniorGhost:FurBaby32@cluster0.w0f6fzl.mongodb.net/portfoliodb",{useNewURLParser: true, useUnifiedTopology: true})
 .then(()=>{
     app.listen(3000,() => {
         console.log("Server is running.")
