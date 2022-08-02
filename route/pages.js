a22265457af3f6d64fec54cb291fbdccb647be03
Ryan Kasper <rhinodino5628@gmail.com>
2022-08-02 11:55:43 -0500

    Changed reg to req.

--- route/pages.js
+++ route/pages.js
@@ -1,38 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const contacts = require('./models/contact')
+const contacts = require("./models/contact");
 
-router.get('/', (reg, res) =>{
-    res.render('index')
-})
-router.get('/Contact', (reg, res) =>{
-    res.render('contact')
-})
-router.get('/Experience', (reg, res) =>{
-    res.render('experience')
-})
-router.get('/Education', (reg, res) =>{
-    res.render('education')
-})
-router.get('/Programming_Languges', (reg, res) =>{
-    console.log("I am running")
-    res.render('Programming_Languges')
-})
+router.get("/", (req, res) => {
+  res.render("index");
+});
+router.get("/Contact", (req, res) => {
+  res.render("contact");
+});
+router.get("/Experience", (req, res) => {
+  res.render("experience");
+});
+router.get("/Education", (req, res) => {
+  res.render("education");
+});
+router.get("/Programming_Languges", (req, res) => {
+  console.log("I am running");
+  res.render("Programming_Languges");
+});
 
-router.post('/submitContact', (req,res) =>{
-    const contact = new contacts({
-        fname: req.body.fname,
-        lname:req.body.lname,
-        phoneNum:req.body.email,
-        email:req.body.email,
-        commentSection: req.body.commentSection
-    });
+router.post("/submitContact", (req, res) => {
+  const contact = new contacts({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    phoneNum: req.body.email,
+    email: req.body.email,
+    commentSection: req.body.commentSection,
+  });
 
-    contacts.collection.insertOne(contact)
-    .then(result => {
-        res.render('contact')
+  contacts.collection
+    .insertOne(contact)
+    .then((result) => {
+      res.render("contact");
     })
-    .catch(err => console.log(err));
-})
+    .catch((err) => console.log(err));
+});
 
-module.exports=router;
+module.exports = router;
